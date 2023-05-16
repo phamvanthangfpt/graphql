af2e4702331b17b25375ad48e495a301bba79652
Your Name <cmc.thang.hoang@careercross.jp>
2023-05-16 09:24:34 +0700

    get countries

--- app/GraphQL/Queries/Countries.php
+++ app/GraphQL/Queries/Countries.php
@@ -16,17 +16,21 @@ final class Countries
             [
                 "id" => 99,
                 "name_en" => "Japan",
+                "name_ja" => "日本",
                 "region" => [
                     "id" => 1,
-                    "name_en" => "Asia"
+                    "name_en" => "Asia",
+                    "name_ja" => "アジア",
                 ]
             ],
             [
                 "id" => 100,
                 "name_en" => "English",
+                "name_ja" => "英語",
                 "region" => [
                     "id" => 2,
-                    "name_en" => "Europe"
+                    "name_en" => "Europe",
+                    "name_ja" => "ヨーロッパ"
                 ]
             ],
         ];
